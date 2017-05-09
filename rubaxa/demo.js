865654a3e99cdc7193a15e22bf45c68501f2588d
vip0_hk@163 <vip0_hk@163.com>
2017-05-09 15:55:34 +0800

    multi修改

--- rubaxa/demo.js
+++ rubaxa/demo.js
@@ -11,13 +11,19 @@
 				{text: 'left2', done: false,groupId:0}
 			];
 
-			$scope.sortableConfig = { group: 'todo', animation: 150 ,
+			$scope.leftConfig = { group: 'todo', animation: 150 ,
 
-			onAdd:function(item){
-				console.log('Add')
-				console.log(item)
-				}
-			}
+			onRemove:function(item){
+				// console.log('1111reomve')
+				// console.log(item.originalEvent.from)
+				$scope.todos.splice(item.oldIndex,1);
+			},
+			
+			};
+
+			'Start End Add Update Sort'.split(' ').forEach(function (name) {
+				$scope.leftConfig['on' + name] = console.log.bind(console, 11111,name);
+			});
 
 			
 		}])
@@ -32,35 +38,36 @@
 
 			// 1,移除当前元素
 			onRemove:function(item){
+				console.log('2222reomve')
 				console.log(item)
 				$scope.todos.splice(item.oldIndex,1);
 			},
 
-			// 2,从别的组添加过来
-			onAdd:function(item){
-				console.log('Add')
-				console.log(item)
-				console.log('从哪来：' + item.model.groupId+'谁：' +item.model.text)
+			// // 2,从别的组添加过来
+			// onAdd:function(item){
+			// 	console.log('Add')
+			// 	console.log(item)
+			// 	console.log('从哪来：' + item.model.groupId+'谁：' +item.model.text)
 
-				var toGId;
-				$scope.todos.forEach(function(ele,index){
-					if (index != item.newIndex) {
-						toGId = ele.groupId;
-						return false;
-					}
-				});
-				console.log('到哪去：'+ toGId + '位置：' + item.newIndex)
-			},
+			// 	var toGId;
+			// 	$scope.todos.forEach(function(ele,index){
+			// 		if (index != item.newIndex) {
+			// 			toGId = ele.groupId;
+			// 			return false;
+			// 		}
+			// 	});
+			// 	console.log('到哪去：'+ toGId + '位置：' + item.newIndex)
+			// },
 
-			// 3,自身的排序
-			onSort(item){
-				console.log('自身排序：'+item.model.groupId+'old:'+item.oldIndex +'new:'+item.newIndex)
-			}
+			// // 3,不是自身的排序，触发排序
+			// onSort(item){
+			// 	console.log('相关列表都受影响：'+item.model.groupId+'old:'+item.oldIndex +'new:'+item.newIndex)
+			// }
 
 		};
-			// 'Start End Add Update Remove Sort'.split(' ').forEach(function (name) {
-			// 	$scope.sortableConfig['on' + name] = console.log.bind(console, name);
-			// });
+			'Start End Add Update Sort'.split(' ').forEach(function (name) {
+				$scope.sortableConfig['on' + name] = console.log.bind(console,222222222, name);
+			});
 		}]);
 })();
 
